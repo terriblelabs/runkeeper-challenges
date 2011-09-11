3bcf9807ce82175284e14e83b9ec38a5000277ca
Jeremy Weiskotten <jeremy@weiskotten.com>
2011-09-11 17:52:14 -0400

    updated mail host to heroku site

--- app/mailers/mailer.rb
+++ app/mailers/mailer.rb
@@ -2,7 +2,7 @@ class Mailer < ActionMailer::Base
   default from: "noreply@terriblelabs.com"
 
   def challenge_notification(challenge)
-    @url = challenge_url(challenge, :host => 'localhost:3000')
+    @url = challenge_url(challenge, :host => 'rk-challenges.herokuapp.com')
     mail(:to => challenge.challengee_email, :subject => "You've been challenged!")
   end
 
