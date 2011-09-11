08cad623d0a8ecd708e03069db971095be5dc0de
Jeremy Weiskotten <jeremy@weiskotten.com>
2011-09-11 13:19:20 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RunkeeperChallenges::Application.config.secret_token = 'af2935f16eeacdf6188d112dbdc0f1e0e85e8a4957083a8432caf2dc10781c9e0ea9efc84f62a7b9cc20209c423fd8309c69d0569d7e39ca5f3086f811cbf22c'
